0f1e12ef419753c7a7ed8a5498be3183b06f5263
Dan Korkelia <dantum@gmail.com>
2020-11-01 17:38:31 +0000

    update view

--- Sources/TokamakApp/main.swift
+++ Sources/TokamakApp/main.swift
@@ -12,19 +12,15 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
-                Text("New Password Generator - iOS App")
+                Text("Swift WebAssembly")
                     .font(.headline)
                     .padding(.bottom)
-                Spacer()
+//                Spacer()
             }
 
             Text("""
-                This minimalist app helps you create, secure passwords for your online and app logins in the fastest possible way.
-
-                Passwords Generator doesn’t store your passwords or email them to you via unsecured email transport. It just lets you use them on your device. You can also use it to generate passwords for your other devices using HandOff.
-
-                When you need a good new password, it's hard to get one quick so you settle for old one which makes your valuable data less secure, especially on mobile device. Passwords Generator app helps you get good random password with helpful presets and with quick double tap you can copy it to wherever you need it!
-                """)
+                Supports Dark mode out of the box.
+                """).foregroundColor(.red)
         }.padding(40)
     }
 }
